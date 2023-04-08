1be84d5254a6cafe8e15760955008a49624b80cc
Neptune27 <vominhtri13@gmail.com>
2023-04-08 20:46:48 +0700

    Init

---
+++ Docker/startup.sh
@@ -0,0 +1,7 @@
+# shellcheck disable=SC2154
+if [ -d "$project_name" ];
+then
+  php $project_name/artisan serve --host=0.0.0.0
+else
+  composer create-project laravel/laravel /var/www/html/${project_name}
+fi
